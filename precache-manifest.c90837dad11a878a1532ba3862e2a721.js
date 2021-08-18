378f2c5316b12b6cc7a18ac865021e8ec4486d70
Yukaii <yukaihuangtw@gmail.com>
2021-08-18 12:50:12 +0000

    Deploy [ci skip]

---
+++ precache-manifest.c90837dad11a878a1532ba3862e2a721.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e24e61640050fd74a5",
+    "url": "/css/app.5934da9f.css"
+  },
+  {
+    "revision": "5520decb4306eeef52ce",
+    "url": "/css/chunk-vendors.b096bbdb.css"
+  },
+  {
+    "revision": "089866e11259e8d37fbab9ae566dc19c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7e24e61640050fd74a5",
+    "url": "/js/app-legacy.bdef9a98.js"
+  },
+  {
+    "revision": "5520decb4306eeef52ce",
+    "url": "/js/chunk-vendors-legacy.3869ed5d.js"
+  },
+  {
+    "revision": "cdc26ad6605b9b9909de246fd71d2fc6",
+    "url": "/manifest.json"
+  }
+]);
